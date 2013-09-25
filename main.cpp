fbd34fac242ff0e68de1cd3a4358de5ca2e94498
my ubuntu <tingleshao@gmail.com>
2013-09-25 15:29:15 -0400

    trying to display an image...

--- main.cpp
+++ main.cpp
@@ -1,11 +1,43 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
+#include "QLabel"
+#include "QGraphicsScene"
+#include "QGraphicsView"
+#include "QGraphicsPixmapItem"
+#include "qlabel.h"
+#include "qpixmap.h";
+#include "qimage.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
-    
+ //   w.show();
+  //  QString *path = new QString("head.png");
+    QImage img("head.png");
+   // QPixmap img = new QPixmap("head.png");
+   QLabel * img_label = new QLabel();
+    img_label->setPixmap(QPixmap::fromImage(img));
+    img_label->setBackgroundRole(QPalette::Base);
+      img_label->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+      img_label->setScaledContents(true);
+
+    img_label->show();
+
+  //  QPixmap image = new QPixmap("blah.jpg");
+
+  //  QLabel imageLabel = new QLabel();
+   // imageLabel.setPixmap(image);
+
+   // mainLayout.addWidget(imageLabel);
+
+
+ //   QGraphicsScene scene;
+  //  QGraphicsView view(&scene);
+  //  QGraphicsPixmapItem item(QPixmap("~/head2.png"));
+  //  scene.addItem(&item);
+  //  view.show();
+
     return a.exec();
 }
+
--- neya.pro
+++ neya.pro
@@ -16,3 +16,7 @@ SOURCES += main.cpp\
 HEADERS  += mainwindow.h
 
 FORMS    += mainwindow.ui
+
+OTHER_FILES += \
+    head2.png \
+    head.png
