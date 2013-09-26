edc1bba06bb280c6e4c3b983b83c0672f8569f21
my ubuntu <tingleshao@gmail.com>
2013-09-26 12:20:23 -0400

    code doing the thresholding

--- main.cpp
+++ main.cpp
@@ -8,14 +8,20 @@
 #include "QGraphicsPixmapItem"
 #include "QVector"
 #include "QRgb"
+#include "QSize"
+
+#include "image_thresholder.h"
+
+#define BACKGROUND_THRESHOLD 90 // define the background thereshold 50
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    int i,j;
     MainWindow w;
     w.show();
     QVector<QRgb> colorTable;
-    for (int i = 0; i < 256; i++) {
+    for (i = 0; i < 256; i++) {
         colorTable.push_back(qRgb(i,i,i));
     }
     QImage *img = new QImage("/home/chong/qt/neya/head.png");
@@ -25,6 +31,24 @@ int main(int argc, char *argv[])
  // img_label->setBackgroundRole(QPalette::Base);
  // img_label->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
  // img_label->setScaledContents(true);
+
+    // make an new image based on the threshold
+    QImage *threshold_img = new QImage(*img);
+    for (i = 0; i < img->width(); i++) {
+        for (j = 0; j < img->height(); j++) {
+            int pixel_intensity = qGray(img->pixel(i,j));
+            if (pixel_intensity < BACKGROUND_THRESHOLD) {
+                threshold_img->setPixel(i,j,qRgb(0,0,0));
+            }
+            else {
+                threshold_img->setPixel(i,j,img->pixel(i,j));
+            }
+        }
+    }
+    QLabel * threshold_img_label = new QLabel();
+    threshold_img_label->setPixmap(QPixmap::fromImage(*threshold_img));
+
+    threshold_img_label->show();
 
     img_label->show();
 
