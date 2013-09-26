3384f025ef521a4c6f8cb3e1e19096c8a63668f5
my ubuntu <tingleshao@gmail.com>
2013-09-26 12:07:13 -0400

    clean code

--- main.cpp
+++ main.cpp
@@ -20,28 +20,13 @@ int main(int argc, char *argv[])
     }
     QImage *img = new QImage("/home/chong/qt/neya/head.png");
     img->setColorTable(colorTable);
-   // QPixmap img = new QPixmap("head.png");
-   QLabel * img_label = new QLabel();
+    QLabel * img_label = new QLabel();
     img_label->setPixmap(QPixmap::fromImage(*img, Qt::AutoColor));
-   // img_label->setBackgroundRole(QPalette::Base);
-    //  img_label->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
-     //img_label->setScaledContents(true);
+ // img_label->setBackgroundRole(QPalette::Base);
+ // img_label->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+ // img_label->setScaledContents(true);
 
     img_label->show();
-
-  //  QPixmap image = new QPixmap("blah.jpg");
-
-  //  QLabel imageLabel = new QLabel();
-   // imageLabel.setPixmap(image);
-
-   // mainLayout.addWidget(imageLabel);
-
-
- //   QGraphicsScene scene;
-  //  QGraphicsView view(&scene);
-  //  QGraphicsPixmapItem item(QPixmap("~/head2.png"));
-  //  scene.addItem(&item);
-  //  view.show();
 
     return a.exec();
 }
